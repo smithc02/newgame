7d0c658378436cb3ced0ba008637449ae6bb3b1b
Caleb <calebfrances@msn.com>
2020-02-12 11:55:59 -0600

    first commit

---
+++ newgame.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ChooseYourOwnAdventure
+{
+  class Program
+  {
+      static void Main(string[] args)
+    {
+      /* THE MYSTERIOUS NOISE */
+
+      // Start by asking for the user's name:
+      Console.Write("What is your name?: ");
+      string name = Console.ReadLine();
+      Console.WriteLine($"Hello, {name}! Welcome to our story.");
+      
+      Console.Write("It begins on a cold rainy night. You're sitting in your room and hear a noise coming from down the hall. Do you go investigate?")
+     
+    }
+  }
+}
